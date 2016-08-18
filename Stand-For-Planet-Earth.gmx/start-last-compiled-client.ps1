cfe27a056e203f6483758d7c9707f71b8360963e
Pierre GILLON <gillon.pierre69@gmail.com>
2016-08-18 15:17:49 +0200

    Ajout d'une saisie du nombre d'instance de jeu à lancer depuis le powershell.

--- Stand-For-Planet-Earth.gmx/start-last-compiled-client.ps1
+++ Stand-For-Planet-Earth.gmx/start-last-compiled-client.ps1
@@ -2,15 +2,20 @@
 $winFile = "Stand-For-Planet-Earth.win"
 $gameStudioRunner = "$env:APPDATA\GameMaker-Studio\Runner.exe"
 
+$count = Read-Host "How many instance of $winFile do you want to start ?"
+
 Write-Output "* Searching the most recent $winfile ..."
 
 $files = @(Get-ChildItem -Path "$env:APPDATA\..\Local\" $winFile -File -Recurse | Sort-Object -Property LastWriteTime | Select-Object -last 1)
 
 if ($files.Length -gt 0) {
     Write-Output "* $winFile has been found here : $($files[0].FullName), date : $($files[0].LastWriteTime)";
-    Write-Output "* Starting the game with game marker studio ...";
-    Start-Process "$gameStudioRunner" " -game `"$($files[0].FullName)`""
+    Write-Output "* Starting $count instances of the game with game marker studio ...";
+
+    for ($i=1; $i -le $count; $i++) {
+        Start-Process "$gameStudioRunner" " -game `"$($files[0].FullName)`""
+    }
 }
 else {
-    Write-Output "* No file found.";
+    Write-Output "* File not found.";
 }
