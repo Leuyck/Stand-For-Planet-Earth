bf52c359f86dac90bad9bdb4c0c9d40d68b2bd2a
Pierre GILLON <gillon.pierre69@gmail.com>
2016-08-28 15:35:04 +0200

    Mise à jour du powershell pour chercher plus rapidement le dernier fichier compilé.

--- Stand-For-Planet-Earth.gmx/start-last-compiled-client.ps1
+++ Stand-For-Planet-Earth.gmx/start-last-compiled-client.ps1
@@ -1,16 +1,16 @@
-
 $winFile = "Stand-For-Planet-Earth.win"
 $gameStudioRunner = "$env:APPDATA\GameMaker-Studio\Runner.exe"
+$gmttt = @(Get-ChildItem -Path "$env:APPDATA\..\Local\" gm_ttt_*);
+$files = New-Object string[] 0
 
-
-
-Write-Output "* Searching the most recent $winfile ..."
-
-$files = @(Get-ChildItem -Path "$env:APPDATA\..\Local\" $winFile -File -Recurse | Sort-Object -Property LastWriteTime | Select-Object -last 1)
+if($gmttt.Length -gt 0) {
+    Write-Output "* Searching the most recent $winfile ...";
+    $files = @(Get-ChildItem -Path "$env:APPDATA\..\Local\$($gmttt[0])\" $winfile -File -Recurse | Sort-Object -Property LastWriteTime | Select-Object -last 1);
+}
 
 if ($files.Length -gt 0) {
     Write-Output "* $winFile has been found here : $($files[0].FullName), date : $($files[0].LastWriteTime)";
-$count = Read-Host "How many instance of $winFile do you want to start ?"
+    $count = Read-Host "How many instance of $winFile do you want to start ?"
     Write-Output "* Starting $count instances of the game with game marker studio ...";
 
     for ($i=1; $i -le $count; $i++) {
